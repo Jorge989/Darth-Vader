1ecd0ff2377f8de1b8e1badd56ebe1465e0a98d4
Jorge989 <attiejorge89@gmail.com>
2021-03-02 09:34:39 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from 'react';
+import "./App.css";
+import axios from 'axios';
+
+export default function App() {
+  // const [planet, setPlanet] = useState({});
+  const [population, setPopulation] = useState('');
+  const [climate, setClimate] = useState('');
+  const [page, setPage] = useState(1);
+  const [name, setName]=useState('');
+  const [diameter, setDiameter]=useState('');
+const [gravity, setGravity] =useState('');
+
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/planets/${page}`).then((response) => {
+      // setPlanet(response.data);
+      // console.log(response.data)
+      setName(response.data.name);
+      setClimate(response.data.climate);
+      setPopulation(response.data.population);
+      setDiameter(response.data.diameter);
+      setGravity(response.data.gravity);
+    });
+  }, [page]);
+
+  function handlePlanets() {
+    setPage(page+1);
+    // setPlanet([...planet, { id: Math.random(planet) }]);
+  }
+  
+  return (
+    <div className="container">
+      <div className="form">
+      <header>
+<h1>Planeta</h1>
+<h2>{name}</h2>
+
+      </header>
+          <ul >
+            <li className="titulo">Gravidade</li>
+            <li>{gravity}</li><hr/>
+            <li className="titulo">Clima</li>
+            <li>{climate}</li>
+            <hr/>
+            <li className="titulo">Populaçao</li>
+              <li>{population}</li><hr/>
+            <li className="titulo">Diametro</li>
+              <li>{diameter}</li>
+          </ul>
+            
+       
+      </div>
+
+      <div className="btn">
+        <button onClick={handlePlanets} className="darkbutton">
+          NEXT
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/services/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+
+let api = axios.create({
+    baseURL:'https://swapi.dev/api/planets/',
+  
+    
+});
+
+
+export default api;
